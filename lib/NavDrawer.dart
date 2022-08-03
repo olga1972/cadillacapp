5c92adc923c69ecff59c0388d3d08e2a68f846c9
Olga Sadyreva <olga.sadyreva@mail.ru>
2022-08-03 17:27:49 +0500

    refactoring NavDrawer and NavDrawerAdmin

--- lib/NavDrawer.dart
+++ lib/NavDrawer.dart
@@ -8,27 +8,8 @@ import 'package:cadillac/pages/news.dart';
 import 'package:cadillac/pages/shop.dart';
 import 'package:cadillac/pages/partners.dart';
 import 'package:cadillac/pages/contacts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-
-//import 'models/user2.dart';
-
-// import 'package:flutter_app_learning/routes.dart';
-// final List<String> menuUser = ["главная", "мой аккаунт", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры", "контакты"];
-// final List<String> menuAdmin = ["главная", "аккаунт члена клуба", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры"];
-// final List<String> icons = ["home.svg", "account.svg", "members.svg", "news.svg", "clubwear.svg", "partners.svg", "contacts.svg"];
-// final List<Widget> pages = [Home(), Account(), Members(), News(), Shop(), Partners(), Contacts()];
-
-// final String assetName = 'assets/images/home.svg';
-// final Widget svg = SvgPicture.asset(
-//     menuUser[0],
-//     semanticsLabel: 'Acme Logo'
-// );
 
 class NavDrawer extends StatefulWidget {
-  //final currentUser = User();
-
-  //const NavDrawer({Key? key, this.currentUser}) : super(key: key);
   const NavDrawer({Key? key}) : super(key: key);
 
   @override
@@ -41,33 +22,15 @@ class _NavDrawerState extends State<NavDrawer> {
   final List<String> menuUser = ["главная", "мой аккаунт", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры", "контакты"];
   final List<String> menuAdmin = ["главная", "аккаунт члена клуба", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры"];
   final List<String> icons = ["home.svg", "account.svg", "members.svg", "news.svg", "clubwear.svg", "partners.svg", "contacts.svg", "contacts.svg"];
-  final List<Widget> pages = [const Home(), Account(), const Members(), const News(), const Shop(), const Partners(), const Contacts()];
-
-  //String userId = '';
-  //String userId = userId;
-//userId =
-  //static get currentUser => null;
-  getdata() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    // preferences.setString('userId', userId);
-    print('get data navdrawer');
-    var userId = preferences.getString('userId')!;
-
-    print(userId);
-    return userId;
-  }
+  final List<Widget> pages = [const Home(), const Account(), const Members(), const News(), const Shop(), const Partners(), const Contacts()];
 
   @override
   Widget build(BuildContext context) {
-
-
     return Align(
         alignment: Alignment.topLeft,
-        child: Container(//Drawer здесь был
-          // margin: const EdgeInsetsDirectional.only(start: 0, end: 0, top: 0, bottom: 2),
+        child: Container(
             width: 344,
             height: 578,
-            // color: const Color(0xFF181c33),
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.only(
                 bottomRight: Radius.circular(15),
@@ -85,64 +48,51 @@ class _NavDrawerState extends State<NavDrawer> {
                   offset: Offset(-2.0, -2.0),
                   blurRadius: 0.0,
                   spreadRadius: 0.0,
-                ),//BoxShadow
+                ), //BoxShadow
               ],
             ),
             // backgroundColor: const Color(0xFF181c33),
-            child: ListView.builder (
-
+            child: ListView.builder(
               padding: const EdgeInsets.only(top: 30, bottom: 10, left: 60, right: 10),
               itemCount: menuUser.length,
               itemBuilder: (BuildContext context, int index) {
                 if (index == 0) {
-                  return Column (
-                      children: [
-                        Container (
-                          width: 230,
-                          alignment: Alignment.topRight,
-                          child: IconButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            icon: const Icon(Icons.close_outlined, color: Colors.white),
-                          ),
+                  return Column(children: [
+                    Container(
+                      width: 230,
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.close_outlined, color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                        title: Text(menuUser[index].toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'CadillacSans',
+                              color: Colors.white,
+                              height: 1.7, //line-height : font-size
+                            )),
+                        leading: SvgPicture.asset(
+                          'assets/images/' + icons[index],
+                          semanticsLabel: 'Icon home',
+                          height: 20.0,
                         ),
-                        ListTile (
-                            title: Text(menuUser[index].toUpperCase(),
-                                style: const TextStyle(
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.normal,
-                                  fontFamily: 'CadillacSans',
-                                  color: Colors.white,
-                                  height: 1.7, //line-height : font-size
-                                )
-                            ),
+                        onTap: () {
+                          // What happens after you tap the navigation item
 
-                            leading: SvgPicture.asset(
-                              'assets/images/' + icons[index],
-                              semanticsLabel: 'Icon home',
-                              height: 20.0,
-                            ),
-
-                            onTap: () {
-                              // What happens after you tap the navigation item
-
-                              setState(() {
-                                mainWidget = pages[index];
-                              });
-                              Navigator.pop(context);
-
-                              getdata();
-                              // Navigator.pushNamed(context, '/' + menuUser[index]);
-                              Route route = MaterialPageRoute(builder: (context) => pages[index]);
-                              Navigator.push(context, route);
-                              //Navigator.pushNamed(context, '/news');
-                              // Route route = MaterialPageRoute(builder: (context) => pages[index] );
-                              // Navigator.push(context,route);
-                            }
-                        )
-                      ]
-                  );
+                          setState(() {
+                            mainWidget = pages[index];
+                          });
+                          Navigator.pop(context);
+                          Route route = MaterialPageRoute(builder: (context) => pages[index]);
+                          Navigator.push(context, route);
+                        })
+                  ]);
                 }
 
                 return ListTile(
@@ -153,42 +103,26 @@ class _NavDrawerState extends State<NavDrawer> {
                           fontFamily: 'CadillacSans',
                           color: Colors.white,
                           height: 1.7, //line-height / font-size
-                        )
-                    ),
-
+                        )),
                     leading: SvgPicture.asset(
                       'assets/images/' + icons[index],
                       semanticsLabel: 'Icon home',
                       height: 20.0,
                     ),
-
                     onTap: () {
                       // What happens after you tap the navigation item
 
-                      print('path');
-                      print(menuUser[index]);
-
+                      debugPrint('path');
+                      debugPrint(menuUser[index]);
 
                       setState(() {
                         mainWidget = pages[index];
                       });
                       Navigator.pop(context);
-                      // Navigator.pushNamed(context, 'members', arguments: User);
-                      // Route route = MaterialPageRoute(builder: (context) => Members());
                       Route route = MaterialPageRoute(builder: (context) => pages[index]);
                       Navigator.push(context, route);
-
-                      // Navigator.pushNamed(context, menuUser[index]);
-
-
-
-                    }
-                );
+                    });
               },
-            )
-
-        )
-    );
+            )));
   }
 }
-
--- lib/NavDrawerAdmin.dart
+++ lib/NavDrawerAdmin.dart
@@ -7,27 +7,8 @@ import 'package:cadillac/pages/shopAdmin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
-
-//import 'models/user2.dart';
-
-// import 'package:flutter_app_learning/routes.dart';
-// final List<String> menuUser = ["главная", "мой аккаунт", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры", "контакты"];
-// final List<String> menuAdmin = ["главная", "аккаунт члена клуба", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры"];
-// final List<String> icons = ["home.svg", "account.svg", "members.svg", "news.svg", "clubwear.svg", "partners.svg", "contacts.svg"];
-// final List<Widget> pages = [Home(), Account(), Members(), News(), Shop(), Partners(), Contacts()];
-
-// final String assetName = 'assets/images/home.svg';
-// final Widget svg = SvgPicture.asset(
-//     menuUser[0],
-//     semanticsLabel: 'Acme Logo'
-// );
-
 class NavDrawerAdmin extends StatefulWidget {
-  //final currentUser = User();
-  final userId;
-  //const NavDrawer({Key? key, this.currentUser}) : super(key: key);
-  const NavDrawerAdmin({Key? key, this.userId,}) : super(key: key);
+  const NavDrawerAdmin({Key? key}) : super(key: key);
 
   @override
   _NavDrawerAdminState createState() => _NavDrawerAdminState();
@@ -36,26 +17,17 @@ class NavDrawerAdmin extends StatefulWidget {
 class _NavDrawerAdminState extends State<NavDrawerAdmin> {
   Widget mainWidget = const HomeAdmin();
 
-
   final List<String> menuAdmin = ["главная", "аккаунт члена клуба", "члены автоклуба", "клубные новости", "клубная атрибутика и одежда", "партнеры"];
   final List<String> icons = ["home.svg", "account.svg", "members.svg", "news.svg", "clubwear.svg", "partners.svg"];
-  final List<Widget> pages = [const HomeAdmin(), AccountAdmin(), const MembersAdmin(), const NewsAdmin(), const ShopAdmin(), const PartnersAdmin()];
-
-  static get userId => null;
-
-  //static get currentUser => null;
-
+  final List<Widget> pages = [const HomeAdmin(), const AccountAdmin(), const MembersAdmin(), const NewsAdmin(), const ShopAdmin(), const PartnersAdmin()];
 
   @override
   Widget build(BuildContext context) {
-
     return Align(
         alignment: Alignment.topLeft,
-        child: Container(//Drawer здесь был
-          // margin: const EdgeInsetsDirectional.only(start: 0, end: 0, top: 0, bottom: 2),
+        child: Container(
             width: 344,
             height: 578,
-            // color: const Color(0xFF181c33),
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.only(
                 bottomRight: Radius.circular(15),
@@ -73,62 +45,49 @@ class _NavDrawerAdminState extends State<NavDrawerAdmin> {
                   offset: Offset(-2.0, -2.0),
                   blurRadius: 0.0,
                   spreadRadius: 0.0,
-                ),//BoxShadow
+                ), //BoxShadow
               ],
             ),
-            // backgroundColor: const Color(0xFF181c33),
-            child: ListView.builder (
-
+            child: ListView.builder(
               padding: const EdgeInsets.only(top: 30, bottom: 10, left: 60, right: 10),
               itemCount: menuAdmin.length,
               itemBuilder: (BuildContext context, int index) {
                 if (index == 0) {
-                  return Column (
-                      children: [
-                        Container (
-                          width: 230,
-                          alignment: Alignment.topRight,
-                          child: IconButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            icon: const Icon(Icons.close_outlined, color: Colors.white),
-                          ),
+                  return Column(children: [
+                    Container(
+                      width: 230,
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.close_outlined, color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                        title: Text(menuAdmin[index].toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'CadillacSans',
+                              color: Colors.white,
+                              height: 1.7, //line-height : font-size
+                            )),
+                        leading: SvgPicture.asset(
+                          'assets/images/' + icons[index],
+                          semanticsLabel: 'Icon home',
+                          height: 20.0,
                         ),
-                        ListTile (
-                            title: Text(menuAdmin[index].toUpperCase(),
-                                style: const TextStyle(
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.normal,
-                                  fontFamily: 'CadillacSans',
-                                  color: Colors.white,
-                                  height: 1.7, //line-height : font-size
-                                )
-                            ),
-
-                            leading: SvgPicture.asset(
-                              'assets/images/' + icons[index],
-                              semanticsLabel: 'Icon home',
-                              height: 20.0,
-                            ),
-
-                            onTap: () {
-                              // What happens after you tap the navigation item
-
-                              setState(() {
-                                mainWidget = pages[index];
-                              });
-                              Navigator.pop(context);
-                              // Navigator.pushNamed(context, '/' + menuUser[index]);
-                              Route route = MaterialPageRoute(builder: (context) => pages[index]);
-                              Navigator.push(context, route);
-                              //Navigator.pushNamed(context, '/news');
-                              // Route route = MaterialPageRoute(builder: (context) => pages[index] );
-                              // Navigator.push(context,route);
-                            }
-                        )
-                      ]
-                  );
+                        onTap: () {
+                          // What happens after you tap the navigation item
+                          setState(() {
+                            mainWidget = pages[index];
+                          });
+                          Navigator.pop(context);
+                          Route route = MaterialPageRoute(builder: (context) => pages[index]);
+                          Navigator.push(context, route);
+                        })
+                  ]);
                 }
 
                 return ListTile(
@@ -139,41 +98,26 @@ class _NavDrawerAdminState extends State<NavDrawerAdmin> {
                           fontFamily: 'CadillacSans',
                           color: Colors.white,
                           height: 1.7, //line-height / font-size
-                        )
-                    ),
-
+                        )),
                     leading: SvgPicture.asset(
                       'assets/images/' + icons[index],
                       semanticsLabel: 'Icon home',
                       height: 20.0,
                     ),
-
                     onTap: () {
                       // What happens after you tap the navigation item
 
-                      print('path');
-                      print(menuAdmin[index]);
+                      debugPrint('path');
+                      debugPrint(menuAdmin[index]);
 
                       setState(() {
                         mainWidget = pages[index];
                       });
                       Navigator.pop(context);
-                      // Navigator.pushNamed(context, 'members', arguments: User);
-                      // Route route = MaterialPageRoute(builder: (context) => Members());
                       Route route = MaterialPageRoute(builder: (context) => pages[index]);
                       Navigator.push(context, route);
-
-                      // Navigator.pushNamed(context, menuUser[index]);
-
-
-
-                    }
-                );
+                    });
               },
-            )
-
-        )
-    );
+            )));
   }
 }
-
